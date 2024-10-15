0dad1182655b84754b03f8f6afc9281479c47e97
avrhahm sason <avi6190i@gmail.com>
2024-10-15 22:08:16 +0300

    .

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 }
 
 const Button = ({ label }: ButtonProps) => {
-    return <button className="bg-red-500 text-green-50 px-4 py-2">{label || "no label provide"}</button>;
+    return <button className="bg-red-500 text-green-500 px-4 py-2">{label || "no label provide"}</button>;
 };
 
 export default Button;
