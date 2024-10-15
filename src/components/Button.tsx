bc13e39c7bf1300cb03ac73d8220334bb178c263
avrhahm sason <avi6190i@gmail.com>
2024-10-15 21:58:27 +0300

    .

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,4 @@
+import '../index.css'
 interface ButtonProps {
     label: string;
 }
--- src/components/Checkboxes.tsx
+++ src/components/Checkboxes.tsx
@@ -1,4 +1,6 @@
+import "../index.css";
 import React, { SetStateAction } from "react";
+
 interface CheckBoxProps {
     id: string;
     checked: boolean;
